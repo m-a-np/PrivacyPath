139be016348c825d098af8f8f4aea71d76efa211
m-a-np <73536459+m-a-np@users.noreply.github.com>
2024-12-20 13:28:28 +0330

    Update Configs.jsx

--- src/components/Configs.jsx
+++ src/components/Configs.jsx
@@ -82,6 +82,23 @@ const Configs = ({
             </Grid>
           </AccordionSummary>
           <AccordionDetails>
+                        <Button
+              onClick={() =>
+                handleCopyToClipboard(filteredLinks.join("\n"), -1, t)
+              }
+              sx={{
+                width: "100%",
+                background: theme.colors.glassColor,
+                color: "#fff",
+                borderRadius: "16px",
+                border: "1px solid #48444a4f",
+                "&:hover": {
+                  background: "rgba(0, 0, 0, 0.3)",
+                },
+              }}
+            >
+              {t("copyAll")}
+            </Button>
             <List>
               {filteredLinks?.map((config, index) => {
                 const title = extractNameFromConfigURL(config);
@@ -129,23 +146,7 @@ const Configs = ({
                 );
               })}
             </List>
-            <Button
-              onClick={() =>
-                handleCopyToClipboard(filteredLinks.join("\n"), -1, t)
-              }
-              sx={{
-                width: "100%",
-                background: theme.colors.glassColor,
-                color: "#fff",
-                borderRadius: "16px",
-                border: "1px solid #48444a4f",
-                "&:hover": {
-                  background: "rgba(0, 0, 0, 0.3)",
-                },
-              }}
-            >
-              {t("copyAll")}
-            </Button>
+
           </AccordionDetails>
         </Accordion>
       </Grid>
