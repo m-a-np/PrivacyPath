44ac1a56d7cd5ba2c7bbb72594809accc9ea67a9
m-a-np <73536459+m-a-np@users.noreply.github.com>
2024-12-20 14:37:39 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -82,7 +82,7 @@ function App() {
 
   const title = data?.username
     ? `${data.username} `
-    : `${import.meta.env.VITE_BRAND_NAME || "PrivcyPath"} `;
+    : `${import.meta.env.VITE_BRAND_NAME || "Ourenus"} Sub Info`;
 
   const isOffSections = useMemo(() => {
     try {
