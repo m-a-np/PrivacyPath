905495c1fc5026b6caceb5b8500437ace84cd439
m-a-np <73536459+m-a-np@users.noreply.github.com>
2025-02-27 23:26:53 +0330

    Update Theme.jsx

--- src/theme/Theme.jsx
+++ src/theme/Theme.jsx
@@ -5,8 +5,8 @@ const commonColors = {
   black: "rgba(0, 0, 0, 1)",
   transparentWhite: "rgba(255, 255, 255, 0.3)",
   transparentBlack: "rgba(0, 0, 0, 0.1)",
-  darkGray: "rgb(34 36 41)",
-  lightGray: "rgb(235 232 253)",
+  darkGray: "rgba(38, 42, 62, 1)",
+  lightGray: "rgba(243, 244, 254, 1)",
   lightPurple: "rgba(156, 39, 176, 1)",
   darkPurple: "rgba(106, 27, 154, 1)",
 };
@@ -18,29 +18,155 @@ const colors = {
     purpleLight: commonColors.lightPurple,
     purpleDark: commonColors.darkPurple,
     low: {
-      colors: { light: ["#9589a5", "#983ce4"], dark: ["#9589a5", "#983ce4"] },
-      background: "#973ce32b",
-      typographyGradient: "linear-gradient(0deg, #9589a5, #983ce4)",
+      colors: {
+        light: ["rgba(255, 102, 102, 1)", "rgba(153, 0, 0, 1)"],
+        dark: ["rgba(255, 102, 102, .5)", "rgba(153, 0, 0, .9)"],
+      },
+      background: "rgba(214, 194, 35, 0.2)",
+      typographyGradient:
+        "linear-gradient(0deg, rgba(255, 102, 102, 1), rgba(153, 0, 0, 1))",
     },
     medium: {
-      colors: { light: ["rgb(249 175 63)"], dark: ["rgb(249 175 63)"] },
+      colors: {
+        light: ["rgba(207, 110, 22, 1)", "rgba(255, 165, 0, 1)"],
+        dark: ["rgba(255, 255, 102, 0.4)"],
+      },
       background: "rgba(214, 194, 35, 0.2)",
-      typographyGradient: "linear-gradient(0deg, rgba(214, 194, 35, 1), rgba(255, 165, 0, 1))",
+      typographyGradient:
+        "linear-gradient(0deg, rgba(214, 194, 35, 1), rgba(255, 165, 0, 1))",
     },
+    high: {
+      colors: {
+        light: ["rgba(144, 238, 144, 1)", "rgba(0, 100, 0, 1)"],
+        dark: ["rgba(144, 238, 144, 0.6)"],
+      },
+      background: "rgba(144, 238, 144, 0.2)",
+      typographyGradient:
+        "linear-gradient(0deg, rgba(144, 238, 144, 1), rgba(0, 100, 0, 1))",
+    },
+  },
+  rgb: {
+    lightShadow: "rgba(0, 0, 0, 0.1)",
+    darkShadow: "rgba(255, 255, 255, 0.1)",
   },
   apps: {
     light: "rgba(72, 76, 112, 1)",
-    dark: "rgb(117 122 166 / 53%)",
+    dark: "rgba(117, 122, 166, 1)",
+    priceBtn: {
+      free: {
+        btn: { dark: "rgba(26, 41, 39, 1)", light: "rgba(226, 241, 239, 1)" },
+        text: {
+          dark: "rgba(136, 192, 166, 1)",
+          light: "rgba(108, 185, 173, 1)",
+        },
+      },
+      paid: {
+        btn: {
+          dark: "rgba(255, 25, 25, 0.65)",
+          light: "rgba(255, 25, 25, 0.65)",
+        },
+        text: { dark: commonColors.white, light: commonColors.white },
+      },
+      ad: {
+        btn: {
+          dark: "rgba(255, 255, 255, 0.65)",
+          light: "rgba(255, 255, 255, 0.65)",
+        },
+        text: { dark: commonColors.black, light: commonColors.black },
+      },
+    },
   },
   configs: {
     light: commonColors.white,
-    dark: "rgb(72 76 122 / 59%)",
+    dark: "rgba(72, 76, 122, 1)",
+    revert: {
+      dark: commonColors.white,
+      light: "rgba(72, 76, 122, 1)",
+    },
   },
   capsuleBtn: {
-    active: { dark: commonColors.white, light: "rgba(82, 88, 125, 1)" },
-    notActive: { dark: commonColors.white, light: "rgba(121, 124, 146, .44)" },
-    background: { dark: "rgb(72 76 111 / 44%)", light: commonColors.lightGray },
-    slider: "rgba(143, 141, 179, 0.4)",
+    active: {
+      dark: commonColors.white,
+      light: "rgba(82, 88, 125, 1)",
+    },
+    notActive: {
+      dark: commonColors.white,
+      light: "rgba(121, 124, 146, 1)",
+    },
+    background: {
+      dark: "rgba(72, 76, 111, 1)",
+      light: commonColors.lightGray,
+    },
+    slider: "rgba(143, 141, 179, 0.6)",
+  },
+  box: {
+    dark: "rgba(72, 76, 111, 1)",
+    light: commonColors.white,
+    border: {
+      dark: "",
+      light: "1px solid rgba(255, 255, 255, 0.42)",
+    },
+  },
+  userBox: {
+    statusBtn: {
+      btn: {
+        active: {
+          dark: "rgba(26, 41, 39, 1)",
+          light: "rgba(226, 241, 239, 1)",
+        },
+        expired: {
+          dark: "rgba(102, 0, 0, 1)",
+          light: "rgba(255, 153, 153, 1)",
+        },
+        onHold: {
+          dark: "rgba(76, 0, 153, 1)",
+          light: "rgba(204, 153, 255, 1)",
+        },
+        disabled: {
+          dark: "rgba(34, 34, 34, 1)",
+          light: "rgba(128, 128, 128, 1)",
+        },
+      },
+      text: {
+        active: {
+          dark: "rgba(136, 192, 166, 1)",
+          light: "rgba(108, 185, 173, 1)",
+        },
+        expired: {
+          dark: "rgba(255, 255, 255, 1)",
+          light: "rgba(255, 255, 255, 1)",
+        },
+        onHold: {
+          dark: "rgba(255, 255, 255, 1)",
+          light: "rgba(255, 255, 255, 1)",
+        },
+        disabled: {
+          dark: "rgba(255, 255, 255, 1)",
+          light: "rgba(77, 77, 77, 1)",
+        },
+      },
+    },
+    logoColor: {
+      dark: "rgba(192, 192, 192, 1)",
+      light: "rgba(83, 72, 141, 1)",
+    },
+    supportBox: {
+      dark: "rgba(25, 40, 160, 1)",
+      light: "rgba(50, 77, 221, 1)",
+    },
+  },
+  glassColor: commonColors.transparentWhite,
+  BWColor: {
+    light: commonColors.black,
+    dark: commonColors.white,
+  },
+  BWColorRevert: {
+    light: commonColors.white,
+    dark: commonColors.black,
+  },
+  grayColor: {
+    light: "rgba(84, 84, 84, 1)",
+    dark: "rgba(204, 204, 204, 1)",
   },
   background: {
     dark: commonColors.darkGray,
@@ -61,35 +187,55 @@ const getTheme = (isDarkMode) =>
       MuiAccordion: {
         styleOverrides: {
           root: {
-            fontFamily: "'Lalezar', sans-serif",
+            backdropFilter: "blur(8px)",
+            borderRadius: "16px !important",
+            border: isDarkMode ? "1px solid rgba(255, 255, 255, 0.42)" : "",
+            color: isDarkMode ? commonColors.black : commonColors.white,
+            fontWeight: "bold",
+            fontFamily: "'Vazirmatn', sans-serif",
+            "&:before": {
+              display: "none",
+            },
           },
         },
       },
+
       MuiButton: {
         styleOverrides: {
           root: {
-            fontFamily: "'Lalezar', sans-serif",
+            borderRadius: "50px",
+            fontFamily: "'Vazirmatn', sans-serif",
           },
         },
       },
       MuiListItem: {
         styleOverrides: {
           root: {
-            fontFamily: "'Lalezar', sans-serif",
+            cursor: "pointer",
+            direction: "ltr",
+            marginBottom: ".5rem",
+            backdropFilter: "blur(8px)",
+            borderRadius: "12px !important",
+            border: "1px solid rgba(72, 68, 74, 0.31)",
+            fontWeight: "bold",
+            fontFamily: "'Vazirmatn', sans-serif",
+            "&:hover": {
+              background: commonColors.transparentBlack,
+            },
           },
         },
       },
       MuiTypography: {
         styleOverrides: {
           root: {
-            fontFamily: "'Lalezar', sans-serif",
+            fontFamily: "'Vazirmatn', sans-serif",
           },
         },
       },
       MuiGrid: {
         styleOverrides: {
           root: {
-            fontFamily: "'Lalezar', sans-serif",
+            fontFamily: "'Vazirmatn', sans-serif",
           },
         },
       },
